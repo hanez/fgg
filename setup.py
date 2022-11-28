5e97b9de3b672117334618fcaa2cac8d48c0ad63
Johannes Findeisen <you@hanez.org>
2022-11-28 13:32:41 +0100

    Added setup.py stub.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='fgg',
+    version='0.1.4',
+    packages=[''],
+    url='https://hanez.org/project/fgg/',
+    license='MIT',
+    author='Johannes Findeisen',
+    author_email='you@hanez.org',
+    description='A free gallery generator for static site generators like Hugo, Jekyll, Nikola '
+                'etc. using Flickr as data source.'
+)
